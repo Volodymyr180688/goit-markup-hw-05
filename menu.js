60da6f25b91fad2f5bce110cd519b906f0039fe0
Volodymyr180688 <shurik180688@gmail.com>
2023-12-04 21:18:11 +0200

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,15 +1,15 @@
 const refs = { 
     
-    openMenuBtn: document.querySelector("[data-menu-open]"),
-    closeMenuBtn: document.querySelector("[data-menu-close]"),
-    menu: document.querySelector("[data-menu]"),
+    openModalBtn: document.querySelector("[data-menu-open]"),
+    closeModalBtn: document.querySelector("[data-menu-close]"),
+    modal: document.querySelector("[data-menu]"),
   };
 
-  refs.openMenuBtn.addEventListener("click", toggleMenu);
-refs.closeMenuBtn.addEventListener("click", toggleMenu);
+  refs.openModalBtn.addEventListener("click", toggleModal);
+refs.closeModalBtn.addEventListener("click", toggleModal);
   
  
-function toggleMenu() {
-  refs.menu.classList.toggle("menu-open");
+function toggleModal() {
+  refs.modal.classList.toggle("menu-open");
 }
    
